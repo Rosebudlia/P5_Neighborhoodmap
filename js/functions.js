3a81252fd9e84353fd51ed93d12ac060a8e731d6
R Tremlett <R Tremlett>
2016-04-10 12:02:22 +0200

    code fixed and foursquare API added

--- js/functions.js
+++ js/functions.js
@@ -6,6 +6,8 @@ var map;
 
 var places = [];
 
+var testarray = ["steve", "hank", "jim", "gary"]
+
 function initMap() {
 
     var mappington = new google.maps.LatLng(52.52, 13.41);
@@ -34,22 +36,26 @@ function initMap() {
     radius: 2000,
     keyword: ['beer garden']
 	}, callback);
-	
 
     marker.setMap(map);
 	
-	var vm = new ViewModel();
-
-	for(var i = 0; i < places.length; i++){
-		vm.srchResults().push(places[i]);
-		console.log('hello');
-	}
-	//vm.srchResults() = places.slice(0);
-	ko.applyBindings(vm);
 	
-	console.log(vm.srchResults());
+
+	// for(var i = 0; i < places.length; i++){
+		// vm.srchResults().push(places[i]);
+		// console.log('hello');
+	// }
+	//vm.srchResults() = places.slice(0);
+	
+	
 };
 
+var vm = new ViewModel();
+
+
+
+
+	
 function ViewModel() {
 	
 	var self = this;
@@ -65,34 +71,56 @@ function ViewModel() {
 
 function callback(results, status) {
 	
-	
 
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
-	  places.push(results[i]);
-
-	
-
+	places.push(results[i]);
+vm.srchResults().push(results[i]);
     }
   }
 
+  ko.applyBindings(vm);
 
-  
 }
 
 function createMarker(place) {
-  var placeLoc = place.geometry.location;
+ 
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
-  //vm.markers().push(marker);
+  
+//	places.push(place.id);
+  
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
+	  
+	  
+	var content = loadFS(place);
+	//var content = loadFS();
+    infowindow.setContent(content);
     infowindow.open(map, this);
   });
   
+}
+
+function loadFS(name){
+	var content;
+	
+
+	var fsUrl = 'https://api.foursquare.com/v2/venues/search?client_id=MVIMITTX1BD0JRLTXC4HBDUC1S1BWMZH2BES15CNNPHSL2CP&client_secret=HPOJUZR2UZWWNKBRPKLSXF5Z2QSOOZXS3QVOOEUCK3ICHX3V&v=20140806&near=berlin, DE&query=' + name.name + '&limit=1&intent=browse'
+	
+	$.getJSON(fsUrl, function(data){
+console.log(data.response.venues[0].stats.checkinsCount);
+        var content = data.response.venues[0].stats.checkinsCount;
+		
+		return content;
+
+    }).error(function(e){
+        var content = 'No foursquare data available for this venue';
+		return content;
+    });
+	
 }
 
 //make the list items rendering function a part of the viewModel
