cee6fe7b8362f52a3330caad28cb5a9f7c16dc3b
R Tremlett <R Tremlett>
2016-04-10 12:06:46 +0200

    add useful comments and clean out broken code

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,7 @@ var map;
 
 var places = [];
 
-var testarray = ["steve", "hank", "jim", "gary"]
+//var testarray = ["steve", "hank", "jim", "gary"]
 
 function initMap() {
 
@@ -40,20 +40,12 @@ function initMap() {
     marker.setMap(map);
 	
 	
-
-	// for(var i = 0; i < places.length; i++){
-		// vm.srchResults().push(places[i]);
-		// console.log('hello');
-	// }
-	//vm.srchResults() = places.slice(0);
-	
 	
 };
 
+//initiate knockout viewmodel - see callback for applyBindings
+
 var vm = new ViewModel();
-
-
-
 
 	
 function ViewModel() {
@@ -67,7 +59,7 @@ function ViewModel() {
 };
 
 
-//set markers for the beer gardens
+//set markers for the beer gardens and apply knockout bindings
 
 function callback(results, status) {
 	
@@ -75,7 +67,7 @@ function callback(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
-	places.push(results[i]);
+	//places.push(results[i]);
 vm.srchResults().push(results[i]);
     }
   }
@@ -85,24 +77,26 @@ vm.srchResults().push(results[i]);
 }
 
 function createMarker(place) {
+	
+	//basic marker creation function
  
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
   
-//	places.push(place.id);
   
   google.maps.event.addListener(marker, 'click', function() {
 	  
 	  
 	var content = loadFS(place);
-	//var content = loadFS();
     infowindow.setContent(content);
     infowindow.open(map, this);
   });
   
 }
+
+//pulls foursquare check-in data via the API when a marker is clicked on
 
 function loadFS(name){
 	var content;
@@ -111,7 +105,7 @@ function loadFS(name){
 	var fsUrl = 'https://api.foursquare.com/v2/venues/search?client_id=MVIMITTX1BD0JRLTXC4HBDUC1S1BWMZH2BES15CNNPHSL2CP&client_secret=HPOJUZR2UZWWNKBRPKLSXF5Z2QSOOZXS3QVOOEUCK3ICHX3V&v=20140806&near=berlin, DE&query=' + name.name + '&limit=1&intent=browse'
 	
 	$.getJSON(fsUrl, function(data){
-console.log(data.response.venues[0].stats.checkinsCount);
+	console.log(data.response.venues[0].stats.checkinsCount);
         var content = data.response.venues[0].stats.checkinsCount;
 		
 		return content;
